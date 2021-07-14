108f9106b6b3a25b2fb3d06b158d0bb1b397c9df
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2021-07-14 12:48:03 -0500

    exercise solved

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -2,49 +2,53 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import convert from '../converter';
 import axios from 'axios';
+import './Converter.css';
+export default function SearchInput() {
 
-export default function SearchInput(){
-
-    const url="https://cors-anywhere.herokuapp.com/https://tranquil-spire-80509.herokuapp.com/uploadAnswer";
-    const [decimalNumber,setDecimalNumber]=useState("");
-    const [romanNumber,setRomanNumber] = useState("");
-    const [error,setError] = useState("");
-    function handleChange(e){
+    const url = "https://cors-anywhere.herokuapp.com/https://tranquil-spire-80509.herokuapp.com/uploadAnswer";
+    const [decimalNumber, setDecimalNumber] = useState("");
+    const [romanNumber, setRomanNumber] = useState("");
+    const [error, setError] = useState("");
+    function handleChange(e) {
         setDecimalNumber(e.target.value);
     }
-    function submit(){
-        if(/^[0-9]+$/.test(decimalNumber)){
+    function submit() {
+        if (/^[0-9]+$/.test(decimalNumber)) {
             setRomanNumber(convert(decimalNumber));
-        }else{
+        } else {
             setError("El campo debe contener sólo valores numéricos");
         }
     }
-    async function post(){
-       try{
-        let res= await axios({
-            method:'post',
-            url:url,
-            data:{
-                input:decimalNumber,
-                output:romanNumber,
-                name:"Mauricio Espinosa Flores"
-            }
-        })
-        console.log(res);
-       }catch(err){
-           console.log("Can't post the solution: "+err);
-       }
+    async function post() {
+        try {
+            let res = await axios({
+                method: 'post',
+                url: url,
+                data: {
+                    input: decimalNumber,
+                    output: romanNumber,
+                    name: "Mauricio Espinosa Flores"
+                }
+            })
+            console.log(res);
+        } catch (err) {
+            console.log("Can't post the solution: " + err);
+        }
     }
-    useEffect(()=>{
-        if(romanNumber.length>0){
+    useEffect(() => {
+        if (romanNumber.length > 0) {
             post();
         }
-    },[romanNumber])
-    return(<>
-        <input className="searchinput" onChange={handleChange}/>
-        <input type="submit" onClick={submit}></input>
-        <span>El número romano es: {romanNumber}</span>
-        <br></br>
-        {error.length>0?error:""}
+    }, [romanNumber])
+    return (<>
+        <div className="converter">
+            <div className="exercise">
+                <input className="searchinput" onChange={handleChange} />
+                <input type="submit" onClick={submit}></input>
+                <span>El número romano es: {romanNumber}</span>
+                <br></br>
+                {error.length > 0 ? error : ""}
+            </div>
+        </div>
     </>)
 }
